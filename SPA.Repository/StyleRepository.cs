dc23b4300dac18c55ba250303c8ec8dbad0b61b4
Suzanne Fiona Fernando <zee198774@gmail.com>
2017-11-30 16:09:22 +0530

    Update StyleRepository.cs

--- SPA.Repository/StyleRepository.cs
+++ SPA.Repository/StyleRepository.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using PLM.Data;
+using SPA.Data;
 
-namespace PLM.Repository
+namespace SPA.Repository
 {
     public class StyleRepository : IStyleRepository
     {
-        private PLMQA_8181Entities _dbContext;
+        private Entities _dbContext;
         public StyleRepository(Entities dbContext)
         {
            // _dbContext = dbContext;
