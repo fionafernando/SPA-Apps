473b4fcd6136f5abdc036d4c4cf79d61f41c806b
Suzanne Fiona Fernando <zee198774@gmail.com>
2017-11-30 16:08:56 +0530

    Update IStyleRepository.cs

--- SPA.Repository/IRepository/IStyleRepository.cs
+++ SPA.Repository/IRepository/IStyleRepository.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using PLM.Data;
+using SPA.Data;
 
-namespace PLM.Repository
+namespace SPA.Repository
 {
     public interface IStyleRepository
     {
